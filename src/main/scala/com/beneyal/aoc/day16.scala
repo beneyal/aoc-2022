60e777d8ed9a4eb89da30858d6f5bc0efea6090f
Ben Eyal <ben.eyal.89@gmail.com>
2022-12-19 22:40:11 +0200

    Refactor day 16

--- src/main/scala/com/beneyal/aoc/day16.scala
+++ src/main/scala/com/beneyal/aoc/day16.scala
@@ -129,16 +129,8 @@ object day16:
   def getMaxPressure(graph: WeightedGraph, source: Valve, withElephant: Boolean = false): Int =
     val timeLimit = if withElephant then 26 else 30
 
-    // Since this whole function is based on dynamic programming, create a mapping between vertices and array indices
-    val nodesWithIndex = graph.keys.toList.sortBy(_.name).zipWithIndex
-    val idxToVertex    = nodesWithIndex.map { case (v, i) => i -> v }.toMap
-    val vertexToIdx    = nodesWithIndex.map { case (v, i) => v -> i }.toMap
-
-    /** Find maximum pressure possible in the graph using dynamic programming
+    /** Find maximum pressure possible in the graph using breadth-first traversal
       *
-      * @param table
-      *   A (|V| * timeLimit) table of numbers representing the best pressure possible if we are starting from a vertex
-      *   `v` and have time limit `timeLimit`.
       * @param q
       *   Queue for the breath-first traversal. Every element is a QueueState consisting of the same components as in
       *   `TunnelTraversal`, but represent an unfinished traversal
@@ -152,7 +144,6 @@ object day16:
       */
     @tailrec
     def loop(
-        table: Vector[Vector[Int]],
         q: Queue[QueueState],
         responsibilities: Set[Valve],
         result: List[TunnelTraversal]
@@ -161,7 +152,7 @@ object day16:
       else
         val (s, q0) = q.dequeue
         val v       = s.valve
-        val i       = vertexToIdx(v)
+
         // Get all neighbors that reaching them is possible within the time limit and haven't been seen yet
         val neighbors = graph(v).filter { case (u, w) =>
           responsibilities(u) && (w + s.time) <= timeLimit && !s.path.map(_._1).contains(u)
@@ -170,52 +161,33 @@ object day16:
         // No more neighbors, we're done with this traversal
         if neighbors.isEmpty then
           val tt = TunnelTraversal(timeLimit, s.time, s.sum, v, s.path)
-          loop(table, q0, responsibilities, tt :: result)
+          loop(q0, responsibilities, tt :: result)
         else
-          // For each neighbor, check if taking the path to it is better than the existing maximum pressure
-          val m0 = neighbors.foldLeft(table) { case (acc, (u, w)) =>
-            val j = vertexToIdx(u)
-            val t = s.time + w
-
-            if t >= timeLimit then acc
-            else
-              val existingPressure = acc(j)(t)
-              val newPressure      = (s.sum + v.pressure) * w
-              acc.updated(j, acc(j).updated(t, List(newPressure, existingPressure).max))
-          }
-
           // Put all the neighbors in the queue with their updated state
-          val q1 = q0.enqueueAll(neighbors.map { case (u, w) =>
+          val newQueue = q0.enqueueAll(neighbors.map { case (u, w) =>
             QueueState(u, (s.time + w), u.pressure + s.sum, (v -> w) :: s.path)
           })
 
-          loop(m0, q1, responsibilities, result)
+          loop(newQueue, responsibilities, result)
 
-    val initialTable = Vector.fill(graph.size)(Vector.fill(timeLimit)(0))
-    // Add the known vertices to the initial zero-initialized table
-    val table = graph(source).foldLeft(initialTable) { case (acc, (v, w)) =>
-      val i = vertexToIdx(v)
-      acc.updated(i, acc(i).updated(w, v.pressure))
-    }
-
-    val q                 = Queue(QueueState(source, 0, 0, List.empty))
-    val keysWithoutSource = graph.keySet - source
+    val q                     = Queue(QueueState(source, 0, 0, List.empty))
+    val verticesWithoutSource = graph.keySet - source
 
     if withElephant then
       // Collect assignments of vertices to human and elephant, take only half (as the other half is symmetrical)
       // and, as a heuristic, keep only assignments where the vertices are distributed evenly
-      val assignments = keysWithoutSource
+      val assignments = verticesWithoutSource
         .subsets()
-        .map(s => (s, keysWithoutSource -- s))
-        .takeWhile(_._1.size <= keysWithoutSource.size / 2)
+        .map(s => (s, verticesWithoutSource -- s))
+        .takeWhile(_._1.size <= verticesWithoutSource.size / 2)
         .filter { case (s1, s2) => math.abs(s1.size - s2.size) <= 1 }
 
       // For each (human, elephant) assignments, take the best standalone traversal of each,
       // combine the traversals to get the best pressure for this pair, then take the maximum
       // of all options
       assignments.map { case (humanAssignment, elephantAssignment) =>
-        val humanTraversals       = loop(table, q, humanAssignment, List.empty)
-        val elephantTraversals    = loop(table, q, elephantAssignment, List.empty)
+        val humanTraversals       = loop(q, humanAssignment, List.empty)
+        val elephantTraversals    = loop(q, elephantAssignment, List.empty)
         val bestHumanTraversal    = humanTraversals.maxBy(_.pressure)
         val bestElephantTraversal = elephantTraversals.maxBy(_.pressure)
         bestHumanTraversal + bestElephantTraversal
@@ -223,7 +195,7 @@ object day16:
     else
       // No elephants around, make a standalone traversal on all vertices,
       // returning the maximum
-      loop(table, q, graph.keySet, List.empty).map(_.pressure).max
+      loop(q, graph.keySet, List.empty).map(_.pressure).max
 
   def main(args: Array[String]): Unit =
     val (graph, source) = readInput()
